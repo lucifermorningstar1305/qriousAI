85611ab95078183f13e6d1619f6019c29c2b3c40
lucifermorningstar1305 <adityam.ghosh@gmail.com>
2023-10-29 04:09:27 +1300

    Added: litetransformers

---
+++ models/text_conv_models.py
@@ -0,0 +1,104 @@
+"""
+@author: Adityam Ghosh
+Date: 10-15-2023
+
+"""
+from typing import List, Tuple, Optional
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class LightConv(nn.Module):
+
+    """ Implementation of LightConv from: https://github.com/facebookresearch/fairseq/blob/main/fairseq/modules/lightweight_convolution.py """
+
+    def __init__(self, input_dim: int, num_heads: int, kernel_size: int, padding: int, weight_softmax: bool, bias: bool, dropout_rate: float):
+
+        super().__init__()
+
+        assert input_dim % num_heads == 0, f"Expected the number of heads to be divisible by the dimension of the model or the number of channels. Found input_dim={input_dim} and H={num_heads}"
+
+        self.num_heads = num_heads
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.weight_softmax = weight_softmax
+        self.dropout_rate = dropout_rate
+
+        self.weight = nn.Parameter(torch.Tensor(num_heads, 1, kernel_size))
+
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(input_dim))
+        else:
+            self.bias = None
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.weight)
+
+        if self.bias:
+            nn.init.constant_(self.bias, 0.0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        A implementation of the forward function for the LightConv Module
+
+        Parameters:
+        ----------
+        :param x: tensor of size (B x T x C) 
+
+        Returns:
+        --------
+        tensor of size (B x T x C)
+        """
+
+        B, T, C = x.size()
+        H = self.num_heads
+
+        weight = self.weight
+
+        if self.weight_softmax:
+            weight = F.softmax(weight, dim=-1)
+
+        weight = F.dropout(weight, p=self.dropout_rate,
+                           training=True, inplace=False)
+
+        x = x.view(-1, H, T)
+
+        output = F.conv1d(x, weight=weight,
+                          padding=self.padding, groups=self.num_heads)
+
+        output = output.view(B, T, C)
+
+        if self.bias is not None:
+            x += self.bias.view(1, -1, 1)
+
+        return output
+
+
+class LightWeightConvBlock(nn.Module):
+    def __init__(self, input_dim: int, num_heads: Optional[int] = 1, kernel_size: Optional[int] = 1, padding: Optional[int] = 0,
+                 weight_softmax: Optional[bool] = True,
+                 bias: Optional[bool] = False, dropout_rate: Optional[float] = .2):
+
+        super().__init__()
+
+        self.proj = nn.Linear(in_features=input_dim, out_features=2*input_dim)
+
+        self.lconv = LightConv(input_dim=input_dim, num_heads=num_heads, kernel_size=kernel_size,
+                               padding=padding, weight_softmax=weight_softmax, bias=bias, dropout_rate=dropout_rate)
+
+        self.fc = nn.Linear(in_features=input_dim, out_features=input_dim)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        x = self.proj(x)
+        x = F.glu(x, dim=-1)  # Converts the (B, T, 2*C) -> (B, T, C)
+        print(x.shape)
+        x = self.lconv(x)
+        print(x.shape)
+        x = self.fc(x)
+
+        return x
--- models/text_models.py
+++ models/text_models.py
@@ -3,13 +3,70 @@
 Date: 10-15-2023
 
 """
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Dict, Optional, Any
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from models.text_conv_models import LightWeightConvBlock
+from models.text_transformers import TransformersEncoderBlock, PositionalEncoding
 
-class LightConv(nn.Module):
-    def __init__(self):
-        pass
+
+class LiteTransformerBlock(nn.Module):
+    def __init__(self, config: Dict):
+        super().__init__()
+
+        self.embed_dim = config["embedding_dim"]
+
+        self.embedding = nn.Embedding(
+            num_embeddings=config["num_embeddings"], embedding_dim=config["embedding_dim"])
+
+        module_dict = nn.ModuleDict({
+            "trans_encoder": TransformersEncoderBlock(input_dim=config["embedding_dim"], num_heads=config["transformer_encoder"]["num_heads"],
+                                                      dropout_rate=config["transformer_encoder"]["dropout_rate"]),
+
+            "lconv_block": LightWeightConvBlock(input_dim=config["embedding_dim"], num_heads=config["lconv"]["num_heads"],
+                                                kernel_size=config["lconv"]["kernel_size"], padding=config["lconv"]["padding"],
+                                                weight_softmax=config["lconv"]["weight_softmax"], dropout_rate=config["lconv"]["dropout_rate"])
+        })
+
+        self.n_blocks = nn.ModuleList([
+            module_dict for _ in range(config["n_blocks"])
+        ])
+
+        self.pos_encoding = PositionalEncoding(
+            embed_dim=config["embedding_dim"], max_seq_length=config["max_seq_length"])
+
+        self.ffn = nn.Linear(
+            in_features=config["embedding_dim"], out_features=config["output_embedding_dim"])
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        An implementation of the LiteTransformerBlock Forward function
+
+        Parameters:
+        -----------
+        :param x: a tensor of shape (B x T)
+
+        Returns:
+        --------
+        a tensor of shape (B x T x C)
+        """
+        x = self.embedding(x)
+
+        for block in self.n_blocks:
+
+            x_left = x[:, :, :self.embed_dim]
+            x_right = x[:, :, self.embed_dim:]
+
+            x_left_out = block["trans_encoder"](x_left)
+            x_right_out = block["lconv_block"](x_right)
+
+            concat_x = torch.concat([x_left_out, x_right_out], dim=-1)
+
+            x = concat_x
+
+        x = self.ffn(x)
+
+        return x
