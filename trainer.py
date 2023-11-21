7be4234f9fbeed215fcd2ac49955d926aa3b8e84
lucifermorningstar1305 <adityam.ghosh@gmail.com>
2023-11-21 16:15:15 +1300

    Modified Dataset for Training

--- trainer.py
+++ trainer.py
@@ -87,10 +87,9 @@ class LitMobileCLiP(pl.LightningModule):
         self, batch: torch.Tensor, batch_idx: torch.Tensor
     ) -> torch.Tensor:
         # print(batch)
-        img, txt, neg_img, neg_txt = (
+        img, txt, neg_txt = (
             batch["img"],
             batch["txt"],
-            batch["neg_img"],
             batch["neg_txt"],
         )
 
@@ -107,7 +106,7 @@ class LitMobileCLiP(pl.LightningModule):
             img,
             txt_input_ids,
             txt_attn_mask,
-            neg_image=neg_img,
+            neg_image=img,
             neg_text=neg_txt_input_ids,
             neg_attn_mask=neg_txt_attn_mask,
         )
@@ -207,3 +206,192 @@ class LitMobileCLiP(pl.LightningModule):
         self, text_tensor: torch.Tensor, attn_mask: Optional[torch.Tensor] = None
     ) -> torch.Tensor:
         return self.clip_model.encode_text(text_tensor, attn_mask=attn_mask)
+
+
+# class LitMobileCLiP(pl.LightningModule):
+#     def __init__(self, config: Dict):
+#         super().__init__()
+
+#         self.cfg = config
+
+#         self.clip_model = MobileCLiP(config)
+#         self.criterion = JSDInfoMaxLoss()
+
+#         self.img_prior_d, self.txt_prior_d = None, None
+#         if self.cfg["image_model"]["prior"]:
+#             self.img_prior_d = PriorDiscriminator(
+#                 inp_dim=self.cfg["image_model"]["output_dim"]
+#             )
+
+#         if self.cfg["text_model"]["prior"]:
+#             self.txt_prior_d = PriorDiscriminator(
+#                 inp_dim=self.cfg["text_model"]["output_dim"]
+#             )
+
+#     def forward(
+#         self,
+#         image: torch.Tensor,
+#         text: torch.Tensor,
+#         attn_mask: Optional[torch.Tensor] = None,
+#         neg_image: Optional[torch.Tensor] = None,
+#         neg_text: Optional[torch.Tensor] = None,
+#         neg_attn_mask: Optional[torch.Tensor] = None,
+#     ) -> Tuple:
+#         model_out = self.clip_model(
+#             image, text, attn_mask, neg_image, neg_text, neg_attn_mask
+#         )
+
+#         return model_out
+
+#     def _compute_loss(self, model_out: Dict) -> torch.Tensor:
+#         # loss_per_img = self.criterion_per_img(yhat_img, targets)
+#         # loss_per_txt = self.criterion_per_txt(yhat_txt, targets.t())
+
+#         IMG_PRIOR = None
+#         TXT_PRIOR = None
+
+#         if self.cfg["image_model"]["prior"]:
+#             img_prior = torch.rand_like(
+#                 model_out["img_feats"], device=model_out["img_feats"].device
+#             )
+#             term_a = torch.log(self.img_prior_d(img_prior)).mean()
+#             term_b = torch.log(1 - self.img_prior_d(model_out["img_feats"])).mean()
+#             IMG_PRIOR = -term_a - term_b
+
+#         if self.cfg["text_model"]["prior"]:
+#             txt_prior = torch.rand_like(
+#                 model_out["txt_feats"], device=model_out["txt_feats"].device
+#             )
+#             term_a = torch.log(self.txt_prior_d(txt_prior)).mean()
+#             term_b = torch.log(1 - self.txt_prior_d(model_out["txt_feats"])).mean()
+#             TXT_PRIOR = -term_a - term_b
+
+#         loss = self.criterion(
+#             model_out["Ej"], model_out["Em"], img_prior=IMG_PRIOR, txt_prior=TXT_PRIOR
+#         )
+#         return loss
+
+#     def _common_steps(
+#         self, batch: torch.Tensor, batch_idx: torch.Tensor
+#     ) -> torch.Tensor:
+#         # print(batch)
+#         img, txt, neg_img, neg_txt = (
+#             batch["img"],
+#             batch["txt"],
+#             batch["neg_img"],
+#             batch["neg_txt"],
+#         )
+
+#         txt_input_ids, txt_attn_mask = (
+#             txt["input_ids"].squeeze(),
+#             txt["attention_mask"].squeeze().float(),
+#         )
+#         neg_txt_input_ids, neg_txt_attn_mask = (
+#             neg_txt["input_ids"].squeeze(),
+#             neg_txt["attention_mask"].squeeze().float(),
+#         )
+
+#         out = self(
+#             img,
+#             txt_input_ids,
+#             txt_attn_mask,
+#             neg_image=neg_img,
+#             neg_text=neg_txt_input_ids,
+#             neg_attn_mask=neg_txt_attn_mask,
+#         )
+
+#         # out = self(
+#         #     img,
+#         #     txt_input_ids,
+#         #     txt_attn_mask,
+#         #     neg_image=None,
+#         #     neg_text=None,
+#         #     neg_attn_mask=None,
+#         # )
+#         loss = self._compute_loss(out)
+
+#         return loss
+
+#     def training_step(self, batch: torch.Tensor, batch_idx: torch.Tensor) -> Dict:
+#         loss = self._common_steps(batch, batch_idx)
+#         self.log(
+#             "train_loss",
+#             loss.item(),
+#             prog_bar=True,
+#             on_step=True,
+#             on_epoch=True,
+#             rank_zero_only=True,
+#             logger=True,
+#             sync_dist=True,
+#         )
+
+#         return {"loss": loss}
+
+#     def validation_step(self, batch: torch.Tensor, batch_idx: torch.Tensor) -> Dict:
+#         loss = self._common_steps(batch, batch_idx)
+
+#         self.log(
+#             "val_loss",
+#             loss.item(),
+#             prog_bar=True,
+#             on_step=False,
+#             on_epoch=True,
+#             rank_zero_only=True,
+#             logger=True,
+#             sync_dist=True,
+#         )
+
+#         return {"val_loss": loss}
+
+#     def empty(self):
+#         return
+#         yield
+
+#     def configure_optimizers(self) -> Any:
+#         params = [
+#             {
+#                 "params": self.clip_model.img_model.parameters(),
+#                 "lr": self.cfg["image_model"]["lr"],
+#                 "weight_decay": self.cfg["image_model"]["weight_decay"],
+#             },
+#             {
+#                 "params": self.clip_model.text_model.parameters(),
+#                 "lr": self.cfg["text_model"]["lr"],
+#                 "weight_decay": self.cfg["text_model"]["weight_decay"],
+#             },
+#             {
+#                 "params": itertools.chain(
+#                     self.clip_model.img_projection.parameters(),
+#                     self.clip_model.text_projection.parameters(),
+#                     self.img_prior_d.parameters()
+#                     if self.img_prior_d is not None
+#                     else self.empty(),
+#                     self.txt_prior_d.parameters()
+#                     if self.txt_prior_d is not None
+#                     else self.empty(),
+#                 ),
+#                 "lr": self.cfg["lr"],
+#                 "weight_decay": self.cfg["weight_decay"],
+#             },
+#         ]
+#         optimizer = torch.optim.Adam(params=params, weight_decay=0.0)
+#         # optimizer = torch.optim.Adam(
+#         #     self.parameters(), lr=self.cfg["lr"], weight_decay=self.cfg["weight_decay"]
+#         # )
+
+#         scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+#             optimizer,
+#             self.cfg["T_0"],
+#             eta_min=self.cfg["min_lr"],
+#             verbose=True,
+#         )
+
+#         return {"optimizer": optimizer, "lr_scheduler": scheduler}
+
+#     def encode_image(self, img_tensor: torch.Tensor) -> torch.Tensor:
+#         return self.clip_model.encode_image(img_tensor)
+
+#     def encode_text(
+#         self, text_tensor: torch.Tensor, attn_mask: Optional[torch.Tensor] = None
+#     ) -> torch.Tensor:
+#         return self.clip_model.encode_text(text_tensor, attn_mask=attn_mask)
--- utility/datasets.py
+++ utility/datasets.py
@@ -186,43 +186,37 @@ class CocoDataset(td.Dataset):
         img_file_root_path = rec["image_path"]
         text = rec["caption"]
 
+        if ".zip" in img_file_root_path:
+            img_file_root_path = img_file_root_path.split(".")[0]
         img_path = os.path.join(img_file_root_path, img_file_name)
+        # pos_rec = self.data.loc[
+        #     (self.data.index != idx) & (self.data["file_name"] == img_file_name)
+        # ]
+        # pos_text = pos_rec["caption"].values[0]
 
         neg_rec = self.data.loc[self.data["file_name"] != img_file_name].sample(n=1)
-        neg_img_file_name = neg_rec["file_name"].values[0]
         neg_text = neg_rec["caption"].values[0]
 
-        neg_img_path = os.path.join(
-            img_file_root_path,
-            neg_img_file_name,
-        )
-
         img = Image.open(img_path).convert("RGB")
-        neg_img = Image.open(neg_img_path).convert("RGB")
 
         if self.resize is not None:
             img = img.resize(
                 (self.resize[1], self.resize[0]), resample=Image.Resampling.BILINEAR
             )
 
-            neg_img = neg_img.resize(
-                (self.resize[1], self.resize[0]), resample=Image.Resampling.BILINEAR
-            )
-
         img = np.array(img)
-        neg_img = np.array(neg_img)
 
         trans_og_data = self.transformations(image=img, caption=text)
-        trans_neg_data = self.transformations(image=neg_img, caption=neg_text)
+        # trans_pos_data = self.transformations(image=img, caption=)
+        trans_neg_data = self.transformations(image=img, caption=neg_text)
 
         img, text = trans_og_data["image"], trans_og_data["caption"]
-        neg_img, neg_text = trans_neg_data["image"], trans_neg_data["caption"]
+        _, neg_text = trans_neg_data["image"], trans_neg_data["caption"]
 
         img = np.transpose(img, (2, 0, 1))
-        neg_img = np.transpose(neg_img, (2, 0, 1))
 
         img = torch.tensor(img, dtype=torch.float)
-        neg_img = torch.tensor(neg_img, dtype=torch.float)
+        # neg_img = torch.tensor(neg_img, dtype=torch.float)
 
         text_obj = self.text_transformations(caption=text)
         neg_text_obj = self.text_transformations(caption=neg_text)
@@ -246,7 +240,7 @@ class CocoDataset(td.Dataset):
             return_tensors="pt",
         )
 
-        return {"img": img, "txt": tok_res, "neg_img": neg_img, "neg_txt": neg_tok_res}
+        return {"img": img, "txt": tok_res, "neg_txt": neg_tok_res}
 
 
 class CocoCLIPDataset(td.Dataset):
@@ -306,3 +300,98 @@ class CocoCLIPDataset(td.Dataset):
         )
 
         return {"img": img, "txt": tok_res}
+
+
+# class CocoDataset(td.Dataset):
+#     def __init__(
+#         self,
+#         data: Any,
+#         text_tokenizer: Callable,
+#         config: Dict,
+#         resize: Optional[Tuple] = None,
+#         transformations: Callable = T.DEFAULT_IMAGE_TRANSFORM,
+#     ):
+#         self.data = data
+#         self.text_tokenizer = text_tokenizer
+#         self.resize = resize
+#         self.transformations = transformations
+#         self.text_transformations = alb.Compose(
+#             [
+#                 T.NormalizeCaption(
+#                     max_caption_length=config["text_model"]["max_seq_length"]
+#                 )
+#             ]
+#         )
+#         self.cfg = config
+
+#     def __len__(self):
+#         return self.data.shape[0]
+
+#     def __getitem__(self, idx: int):
+#         rec = self.data.iloc[idx]
+
+#         img_file_name = rec["file_name"]
+#         img_file_root_path = rec["image_path"]
+#         text = rec["caption"]
+
+#         img_path = os.path.join(img_file_root_path, img_file_name)
+
+#         neg_rec = self.data.loc[self.data["file_name"] != img_file_name].sample(n=1)
+#         neg_img_file_name = neg_rec["file_name"].values[0]
+#         neg_text = neg_rec["caption"].values[0]
+
+#         neg_img_path = os.path.join(
+#             img_file_root_path,
+#             neg_img_file_name,
+#         )
+
+#         img = Image.open(img_path).convert("RGB")
+#         neg_img = Image.open(neg_img_path).convert("RGB")
+
+#         if self.resize is not None:
+#             img = img.resize(
+#                 (self.resize[1], self.resize[0]), resample=Image.Resampling.BILINEAR
+#             )
+
+#             neg_img = neg_img.resize(
+#                 (self.resize[1], self.resize[0]), resample=Image.Resampling.BILINEAR
+#             )
+
+#         img = np.array(img)
+#         neg_img = np.array(neg_img)
+
+#         trans_og_data = self.transformations(image=img, caption=text)
+#         trans_neg_data = self.transformations(image=neg_img, caption=neg_text)
+
+#         img, text = trans_og_data["image"], trans_og_data["caption"]
+#         neg_img, neg_text = trans_neg_data["image"], trans_neg_data["caption"]
+
+#         img = np.transpose(img, (2, 0, 1))
+#         neg_img = np.transpose(neg_img, (2, 0, 1))
+
+#         img = torch.tensor(img, dtype=torch.float)
+#         neg_img = torch.tensor(neg_img, dtype=torch.float)
+
+#         text_obj = self.text_transformations(caption=text)
+#         neg_text_obj = self.text_transformations(caption=neg_text)
+
+#         text = text_obj["caption"]
+#         neg_text = neg_text_obj["caption"]
+
+#         tok_res = self.text_tokenizer(
+#             text,
+#             max_length=self.cfg["text_model"]["max_seq_length"],
+#             padding="max_length",
+#             truncation=True,
+#             return_tensors="pt",
+#         )
+
+#         neg_tok_res = self.text_tokenizer(
+#             neg_text,
+#             max_length=self.cfg["text_model"]["max_seq_length"],
+#             padding="max_length",
+#             truncation=True,
+#             return_tensors="pt",
+#         )
+
+#         return {"img": img, "txt": tok_res, "neg_img": neg_img, "neg_txt": neg_tok_res}
